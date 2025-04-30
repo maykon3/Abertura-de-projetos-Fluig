25a41d45baa0c45fe7bc788f66cc056facd30980
maykon3 <mkn.silva29@gmail.com>
2025-04-30 16:07:42 -0300

    feat: valor total ajustado

--- forms/Solicitação de abertura de projetos/atividade.js
+++ forms/Solicitação de abertura de projetos/atividade.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
     // Verifica em qual estado esta o diagrama e preenche o capo de data com a data do dia.
     if (stateProcess == 0) {
@@ -10,47 +10,94 @@ $(document).ready(function() {
         // Para que os meses fiquem com dois digitos, se o mes for menor que 10, adicionamos um zero a esquerda.
         if (mes < 10) {
             mes = '0' + mes;
-        }else {
-          mes = mes;
+        } else {
+            mes = mes;
         }
         // Para que os dias fiquem com dois digitos, se o dia for menor que 10, adicionamos um zero a esquerda.
         if (dia < 10) {
             dia = '0' + dia;
             var diaMenor = '0' + dia;
-        }else {
+        } else {
             diaMenor = dia;
         }
 
-        var hoje = diaMenor + '/' + mes+ '/' + ano;
-        
+        var hoje = diaMenor + '/' + mes + '/' + ano;
+
         $('#calendario').val(hoje);
     }
-    
+
 })
 
-// Função para pegar o valor do campo de data e colocar no campo de data do formulario.
- function setSelectedZoomItem(selectedItem) {
-    if(selectedItem.inputId == "fornecedor") {
-       $("#valor").val(selectedItem["cnpj"]);
-    }
- }
 
- // Forma de acessar campo zoom em uma tabela pai e filho 
- function setSelectedZoomItem(selectedItem){
+
+// Função para pegar o valor do campo de data e colocar no campo de data do formulario.
+function setSelectedZoomItem(selectedItem) {
+    if (selectedItem.inputId == "fornecedor") {
+        $("#valor").val(selectedItem["cnpj"]);
+    }
+}
+
+// Forma de acessar campo zoom em uma tabela pai e filho 
+function setSelectedZoomItem(selectedItem) {
     const [inputName, inputIndex] = selectedItem.inputName.split("___");
- 
+
     if (inputName === "fornecedor") {
         $(`#valor___${inputIndex}`).val(selectedItem.cnpj);
     }
 }
- 
- // Função para o botao de incluir ao ser clicado add um linha na tabela pai e filho 
- function addLinha(){
-    wdkAddChild("respoTable");   
+function removedZoomItem(removedItem) {
+    const [inputName, inputIndex] = removedItem.inputName.split("___");
+    if (inputName === "fornecedor") {
+        $(`#valor___${inputIndex}`).val("");
+    }
 }
 
-function removeLinha(elemento){
-    fnWdkRemoveChild(elemento); 
+// Função para o botao de incluir ao ser clicado add um linha na tabela pai e filho 
+function addLinha() {
+    wdkAddChild("respoTable");
+    
+    calcularTotal();
+   
+
+}
+
+
+function calcularTotal(idCampo) {
+    var linha = idCampo.split("___")[1]
+
+    $(document).ready(function () {
+
+        var quanFloat = parseFloat($("#quantia___" + linha).val().toString().replace(".", "").replace(",", "."));
+        var uniFloat = parseFloat($("#unitario___" + linha).val().toString().replace(".", "").replace(",", "."));
+
+        var soma = quanFloat * uniFloat;
+
+        $("#total___" + linha).val(soma.toFixed(2).toString().replace(".", ","));
+        totalFinal();
+
+
+    })
+
+}
+
+
+function totalFinal() {
+    var totalGeral = 0;
+    $("input[id^='total___']").each(function () {
+
+        var valor = $(this).val().replace(".", "").replace(",", ".");
+
+        if (!isNaN(valor)) {
+            totalGeral += parseFloat(valor);
+        }
+
+    });
+    $("#valorTotal").val(totalGeral.toFixed(2).toString().replace(".", ","));
+}
+
+function removeLinha(elemento) {
+    fnWdkRemoveChild(elemento);
+    
 }
 
 
